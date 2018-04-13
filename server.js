c60937a5e6b5e0d25348f89fe7277ae92434f62c
Matt Krabacher <matt.krabacher@gmail.com>
2018-04-13 10:52:27 -0700

    corrected some spelling errors and some caseing errors that appeared after deployment

--- server.js
+++ server.js
@@ -18,28 +18,6 @@ var router = express.Router();
 
 
 //routes
-app.get('/allAttacks', function (req, res) {
-    console.log('getting all attacks in server')
-    attacks.find({},function(err, attacks) {
-        if(err){
-            console.log("e0rr0r",)
-        }else{
-            res.json({message:'The Attacks', attacks: attacks})
-        }
-    })
-})
-app.post('/getPet', function (req, res) {
-    console.log('looking for pet in DB')
-    console.log(req.body)
-    Pet.findOne({_id:req.body._id},function(err, pet) {
-        if(pet != null) {
-            console.log('found pet in server')
-            res.json({message:'The pet', pet: pet})
-        } else {
-            res.json({errorMsg: 'dat pet don\'t exists'})
-        }
-    })
-})
 app.post('/mailer', function (req, res) {
     console.log('trying to mail now', req.body)
     var transporter = nodemailer.createTransport({
@@ -65,55 +43,6 @@ app.post('/mailer', function (req, res) {
             res.json({yo: info.response});
         };
     });
-})
-app.post('/likePet', function (req, res) {
-    console.log('likeing pet in server')
-    console.log(req.body)
-    Pet.update({_id: req.body._id}, {
-        likes: req.body.likes + 1,
-    }, function(err, pet) {
-        if(err){
-            console.log("like error", err)
-        }else{
-            res.json({message:`pet liked`, pet: req.body._id})
-        }
-    })
-})
-app.post('/deletePet', function (req, res) {
-    console.log('deleteing Pet in server')
-    Pet.remove({_id: req.body._id}, function(err, user) {
-        if(err){
-            console.log("deleteing error",)
-        }else{
-            res.json({message:`Pet deleted`})
-        }
-    })
-})
-app.post('/addPet', function(req, res) {
-    Pet.find({name: req.body.name}, function(err, pet) {
-        console.log('in server')
-        if(pet.length == 0) {            
-            console.log("creating new pet in server")
-            newPet = new Pet()
-            newPet.name = req.body.name
-            newPet.type = req.body.type
-            newPet.description = req.body.description
-            newPet.skills = req.body.skills
-            newPet.likes = 0
-            console.log(pet)
-            newPet.save(function(err) {
-                if(err){
-                    console.log('new pet error')
-                    res.json({err})
-                }else{
-                    res.json({message:`with ${newPet.fName} added`})
-                    console.log('pet added');
-                }
-            })
-        } else {
-            res.json({error: 'that pet already exists in our DBs'});
-        }
-    })
 })
 app.all("*", (req,res,next) => {
     res.sendFile(path.resolve("./Portfolio/dist/index.html"))
