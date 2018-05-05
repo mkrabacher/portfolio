38c8aa2cbb7a75413d63c7e95bf74fe865be3b73
Matt Krabacher <matt.krabacher@gmail.com>
2018-05-05 15:14:13 -0700

    updated splash page mobile styleing

--- Portfolio/src/assets/js/ballOfString.js
+++ Portfolio/src/assets/js/ballOfString.js
@@ -1,42 +1,43 @@
+$(document).ready(function () {
+    var context = document.getElementById('ball-of-string').getContext('2d');
 
-var context = document.getElementById('content').getContext('2d');
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    var size = d3.min([width, height]);
 
-var width = window.innerWidth;
-var height = window.innerHeight;
-var size = d3.min([width, height]);
+    d3.select('#ball-of-string')
+        .attr('width', width + 'px')
+        .attr('height', height + 'px');
 
-d3.select('#content')
-    .attr('width', width + 'px')
-    .attr('height', height + 'px');
+    context.lineWidth = 0.4;
+    context.strokeStyle = `rgba(255, 255, 255, 0.6)`;
 
-context.lineWidth = 0.4;
-context.strokeStyle = `rgba(255, 255, 255, 0.6)`;
+    var projection = d3.geoOrthographic()
+        .scale(0.45 * size)
+        .translate([0.5 * width, 0.5 * height]);
 
-var projection = d3.geoOrthographic()
-    .scale(0.45 * size)
-    .translate([0.5 * width, 0.5 * height]);
+    var geoGenerator = d3.geoPath()
+        .projection(projection)
+        .context(context);
 
-var geoGenerator = d3.geoPath()
-    .projection(projection)
-    .context(context);
+    var geojson = { type: 'Feature', geometry: { type: 'LineString', coordinates: [] } };
+    function rndLon() { return -180 + Math.random() * 360; }
+    function rndLat() { return -90 + Math.random() * 180; }
+    function addPoint() { geojson.geometry.coordinates.push([rndLon(), rndLat()]) }
 
-var geojson = { type: 'Feature', geometry: { type: 'LineString', coordinates: [] } };
-function rndLon() { return -180 + Math.random() * 360; }
-function rndLat() { return -90 + Math.random() * 180; }
-function addPoint() { geojson.geometry.coordinates.push([rndLon(), rndLat()]) }
+    function update(t) {
+        if (geojson.geometry.coordinates.length < 2000)
+            addPoint();
 
-function update(t) {
-    if (geojson.geometry.coordinates.length < 2000)
-        addPoint();
+        projection.rotate([t / 1000]);
 
-    projection.rotate([t / 1000]);
+        context.clearRect(0, 0, (width), height);
+        context.beginPath();
+        geoGenerator(geojson);
+        context.stroke();
 
-    context.clearRect(0, 0, (width), height);
-    context.beginPath();
-    geoGenerator(geojson);
-    context.stroke();
+        window.requestAnimationFrame(update);
+    }
 
     window.requestAnimationFrame(update);
-}
-
-window.requestAnimationFrame(update);
+})
