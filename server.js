39764f63663eea4fdaedf9b70fc6c34e8f6d2fd5
Matt Krabacher <matt.krabacher@gmail.com>
2018-05-05 17:00:35 -0700

    debugging email service error

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.post('/mailer', function (req, res) {
     transporter.sendMail(mailOptions, function(error, info){
         if(error){
             console.log(error);
-            res.json({yo: 'error'});
+            res.json({yo: error});
         }else{
             console.log('Message sent: ' + info.response);
             res.json({yo: info.response});
